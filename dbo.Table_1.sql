f4ccc46605abfc2cf106b8075e73a3f04753f7b5
keeganibean <56174677+keeganibean@users.noreply.github.com>
2021-10-24 23:42:27 -0500

    first commit

---
+++ dbo.Table_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[OrderID] INT NOT NULL PRIMARY KEY, 
+    [CustomerID] NCHAR(5) NOT NULL, 
+    [OrderDate] DATETIME NULL, 
+    [OrderQuanity] INT NULL, 
+    CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustomerID]) REFERENCES [Customers]([CustomerID])
+)
